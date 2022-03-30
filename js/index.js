76e5678180a10a4c01f9e321d5deef1b45bd19f5
unknown <cdiaz@ggtech.es>
2022-03-29 22:24:45 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+const changeMessage = () => {
+
+    let message = document.getElementById("showMessage");
+    message.innerText= "k preciosos ojos tienes bb ❤️"
+}
+
+
+document.getElementById("showMessage").addEventListener("click", changeMessage);
