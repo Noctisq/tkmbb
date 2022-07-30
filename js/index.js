fcb92fb16910eb47af719aa2cd2d9fcea5eee763
Svitla <svitla@Svitlas-MacBook-Pro.local>
2022-07-30 16:41:26 -0500

    final commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
 
 const changeMessage = () => {
-
     let message = document.getElementById("showMessage");
-    message.innerText= "k preciosos ojos tienes bb ❤️ \n también eres la verch ❤️ "
+
+    if(localStorage.getItem("visited")){ 
+       return message.innerText= "Sé que esto no es la gran cosa, \n pero esto nunca se va a perder o borrar \n siempre va a estar siempre que lo quieras ver \n Me gustas, me encantas y lo más importante: \n te quiero muchísimo ❤️"
+
+
+    }
+    message.innerText= "Lee la hoja❤️"
+    localStorage.setItem("visited", true);
 }
 
+
+const checkIfVisited = () => {
+    let message = document.getElementById("showMessage");
+
+    if(localStorage.getItem("visited")) 
+        return message.innerText = "¿Quieres ser mi novia?❤️"
+
+    return false;
+}
+
+
+checkIfVisited();
 
 document.getElementById("showMessage").addEventListener("click", changeMessage);
